b7c55d8499e283ab19896f298fa40124733c4bb4
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-06 01:55:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Function to simulate rolling a 12-sided die and display the result with animation.
+// Initialize an object to store roll counts
+const rollCounts = {
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+};
+
+// Function to simulate rolling a 12-sided die and update counts.
 function rollDice() {
     const resultElement = document.getElementById('result');
     const rolledValue = Math.floor(Math.random() * 12) + 1;
@@ -18,7 +28,19 @@ function rollDice() {
         }, 10);
     });
 
+    // Update roll count
+    rollCounts[rolledValue] += 1;
+    updateRollCounts();
+    
     return rolledValue;
+}
+
+// Function to update the roll counts in the table
+function updateRollCounts() {
+    for (let i = 2; i <= 7; i++) {
+        const countElement = document.getElementById(`rollCount${i}`);
+        countElement.textContent = rollCounts[i];
+    }
 }
 
 // Attach a click event listener to the button.
