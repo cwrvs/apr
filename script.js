08a1fcd2f3f5c1ab4fcbacfa1f9c8da44169e027
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-05 22:27:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// Function to simulate rolling a 12-sided die and display the result.
+// Function to simulate rolling a 12-sided die and display the result with animation.
 function rollDice() {
-    const result = Math.floor(Math.random() * 12) + 1; // Generates a random number from 1 to 12
-    return result;
+    const resultElement = document.getElementById('result');
+    const resultNumbers = resultElement.querySelectorAll('.number');
+    const rolledValue = Math.floor(Math.random() * 12) + 1;
+
+    // Calculate the translateY value for animation
+    const translateY = -(rolledValue - 1) * 40; // Adjust the multiplier for desired spacing
+
+    // Animate the numbers
+    resultNumbers.forEach((number, index) => {
+        const animationDelay = index * 100; // Adjust the delay for desired animation speed
+        setTimeout(() => {
+            number.style.transform = `translateY(${translateY}px)`;
+        }, animationDelay);
+    });
+
+    return rolledValue;
 }
 
 // Attach a click event listener to the button.
 document.getElementById('rollButton').addEventListener('click', function() {
     const rolledValue = rollDice();
-    const resultElement = document.getElementById('result');
-    
-    if (rolledValue === 7 || rolledValue === 11) {
-        resultElement.textContent = `Unlucky craps! You rolled a ${rolledValue}`;
-    } else {
-        resultElement.textContent = `You rolled a ${rolledValue}`;
-    }
+    setTimeout(() => {
+        document.getElementById('result').textContent = `You rolled a ${rolledValue}`;
+    }, 1000); // Adjust the delay to match the animation duration
 });
