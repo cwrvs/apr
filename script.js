9f5cf243457447c60d2be201acb4da5d35299507
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-05 22:33:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // Function to simulate rolling a 12-sided die and display the result with animation.
 function rollDice() {
     const resultElement = document.getElementById('result');
+    const resultNumbers = resultElement.querySelectorAll('.number');
     const rolledValue = Math.floor(Math.random() * 12) + 1;
 
-    // Remove the existing result content and classes
+    // Remove the existing result content
     resultElement.innerHTML = '';
+
+    // Calculate the translateY value for animation
+    const translateY = -(rolledValue - 1) * 40; // Adjust the multiplier for desired spacing
 
     // Create and append new span elements for each digit
     const digits = rolledValue.toString().split('');
-    digits.forEach((digit) => {
+    digits.forEach((digit, index) => {
         const span = document.createElement('span');
         span.className = 'number';
         span.textContent = digit;
+        span.style.transform = `translateY(${translateY}px)`;
+        span.style.transition = `transform ${(index + 1) * 0.2}s ease-out`; // Adjust the delay for desired animation speed
         resultElement.appendChild(span);
+
+        // Trigger the animation by reading the computed style (force reflow)
+        setTimeout(() => {
+            span.style.transform = 'translateY(0)';
+        }, 10);
     });
 
     return rolledValue;
