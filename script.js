0f7f79ebe2ef8335e9ec4e370aa22dbf3c09b405
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-06 00:58:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,19 @@
-// Function to simulate rolling a 6-sided die and return the result.
-function rollDie() {
-    return Math.floor(Math.random() * 6) + 1;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Random Dice Generator</title>
+</head>
+<body>
+    <div class="dice">
+        <div class="die" id="die1"></div>
+        <div class="die" id="die2"></div>
+    </div>
+    <button id="rollButton">Roll Dice</button>
+    <p>Total: <span id="total">0</span></p>
 
-// Function to roll two dice, update the simulated dice, and display the result.
-function rollDice() {
-    const die1Element = document.getElementById('die1');
-    const die2Element = document.getElementById('die2');
-
-    // Roll two dice
-    const roll1 = rollDie();
-    const roll2 = rollDie();
-
-    // Update the simulated dice
-    updateSimulatedDie(die1Element, roll1);
-    updateSimulatedDie(die2Element, roll2);
-
-    // Calculate the total roll
-    const totalRoll = roll1 + roll2;
-
-    // Display the result message
-    displayResult(totalRoll);
-}
-
-// Function to update the simulated die with dots based on the roll.
-function updateSimulatedDie(dieElement, roll) {
-    // Clear the die
-    dieElement.innerHTML = '';
-
-    // Define dot positions for each face of a 6-sided die
-    const dotPositions = [
-        [],
-        [0],
-        [0, 2],
-        [0, 1, 2],
-        [0, 1, 2, 4],
-        [0, 1, 2, 3, 4],
-    ];
-
-    // Create dots based on the roll
-    dotPositions[roll].forEach((position) => {
-        const dot = document.createElement('div');
-        dot.className = 'dot';
-        dieElement.appendChild(dot);
-    });
-}
-
-// Function to display the result message
-function displayResult(totalRoll) {
-    const outcomeMessageElement = document.getElementById('outcomeMessage');
-    outcomeMessageElement.textContent = `You rolled a total of ${totalRoll}`;
-}
-
-// Attach a click event listener to the button to roll the dice
-document.getElementById('rollButton').addEventListener('click', rollDice);
+    <script src="script.js"></script>
+</body>
+</html>
