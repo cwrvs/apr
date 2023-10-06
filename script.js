f47bfea9541e9b5d2156e7adf2eb9b6cfc0dbbe6
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-06 01:01:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Random Dice Generator</title>
-</head>
-<body>
-    <div class="dice">
-        <div class="die" id="die1"></div>
-        <div class="die" id="die2"></div>
-    </div>
-    <button id="rollButton">Roll Dice</button>
-    <p>Total: <span id="total">0</span></p>
+// Function to simulate rolling a six-sided die
+function rollDie() {
+    return Math.floor(Math.random() * 6) + 1;
+}
 
-    <script src="script.js"></script>
-</body>
-</html>
+// Function to update the dice display
+function updateDice() {
+    const die1 = document.getElementById('die1');
+    const die2 = document.getElementById('die2');
+    
+    const roll1 = rollDie();
+    const roll2 = rollDie();
+    
+    die1.textContent = roll1;
+    die2.textContent = roll2;
+    
+    return roll1 + roll2;
+}
+
+// Function to update the total and tally
+function updateTotal(total, tally) {
+    const totalElement = document.getElementById('total');
+    totalElement.textContent = total;
+
+    tally[total] = (tally[total] || 0) + 1;
+}
+
+const rollButton = document.getElementById('rollButton');
+const tally = {};
+
+rollButton.addEventListener('click', () => {
+    const total = updateDice();
+    updateTotal(total, tally);
+});
+
+// Initial roll
+updateDice();
