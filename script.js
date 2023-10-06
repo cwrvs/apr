35c59809ece6b7cb58ef3fe0c7d2278f5e0ee1b0
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-05 21:00:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Define an array to represent the faces of a standard 6-sided die.
-const diceFaces = [1, 2, 3, 4, 5, 6];
+const diceFaces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // Function to simulate rolling the dice and display the result.
 function rollDice() {
