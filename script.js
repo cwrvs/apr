c80e361b4f958133e9e7d6008f6cf22e46ca86d8
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-06 02:13:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Initialize game variables
-let isPointEstablished = false;
-let point = 0;
-
 // Function to simulate rolling two 6-sided dice and determine the game outcome.
 function rollDice() {
     const resultElement = document.getElementById('result');
+    const rollValueElement = document.getElementById('rollValue'); // Get the element for displaying the roll value
     const rolledValue1 = Math.floor(Math.random() * 6) + 1;
     const rolledValue2 = Math.floor(Math.random() * 6) + 1;
     const total = rolledValue1 + rolledValue2;
@@ -22,31 +19,8 @@ function rollDice() {
     dice2.textContent = rolledValue2;
     resultElement.appendChild(dice2);
 
-    // Check the game outcome based on the rules
-    if (!isPointEstablished) {
-        if (total === 7 || total === 11) {
-            resultElement.textContent = 'Winner! Natural!';
-        } else if (total === 2 || total === 3 || total === 12) {
-            resultElement.textContent = 'Loser! Craps!';
-        } else {
-            isPointEstablished = true;
-            point = total;
-            resultElement.textContent = `Point is ${point}. Keep rolling!`;
-        }
-    } else {
-        if (total === 7) {
-            resultElement.textContent = 'Loser! Point lost.';
-            isPointEstablished = false;
-        } else if (total === point) {
-            resultElement.textContent = 'Winner! Point matched!';
-            isPointEstablished = false;
-        } else {
-            resultElement.textContent = `Roll again for point ${point}.`;
-        }
-    }
-}
+    // Display the roll result
+    rollValueElement.textContent = `You rolled: ${total}`;
 
-// Attach a click event listener to the button.
-document.getElementById('rollButton').addEventListener('click', function() {
-    rollDice();
-});
+    // Rest of the game logic...
+}
