463b77f03c400e720f1f14c1c94920cf451a9370
cwrvs <nick.galanti@gmail.com>
2023-10-05 23:17:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-// Function to simulate rolling a 12-sided die and display the result with animation.
+// Initialize roll counts for each number
+const rollCounts = {
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+};
+
+// Function to simulate rolling a pair of dice and display the result with animation.
 function rollDice() {
     const resultElement = document.getElementById('result');
     const resultNumbers = resultElement.querySelectorAll('.number');
-    const rolledValue = Math.floor(Math.random() * 12) + 1;
+
+    // Roll two six-sided dice
+    const die1 = Math.floor(Math.random() * 6) + 1;
+    const die2 = Math.floor(Math.random() * 6) + 1;
+    const rolledValue = die1 + die2;
 
     // Remove the existing result content
     resultElement.innerHTML = '';
 
     // Calculate the translateY value for animation
-    const translateY = -(rolledValue - 1) * 40; // Adjust the multiplier for desired spacing
+    const translateY = -(rolledValue - 2) * 40; // Adjust the multiplier for desired spacing
 
     // Create and append new span elements for each digit
     const digits = rolledValue.toString().split('');
@@ -29,7 +48,40 @@ function rollDice() {
     return rolledValue;
 }
 
-// Attach a click event listener to the button.
-document.getElementById('rollButton').addEventListener('click', function() {
-    rollDice();
-});
+// Function to determine the outcome of a roll based on craps rules
+function determineRollOutcome(rolledValue) {
+    if (rolledValue === 2 || rolledValue === 3 || rolledValue === 12) {
+        return 'craps';
+    } else if (rolledValue === 7 || rolledValue === 11) {
+        return 'natural';
+    } else {
+        return 'point';
+    }
+}
+
+// Function to display the outcome message
+function displayOutcomeMessage(outcome) {
+    const messageElement = document.getElementById('outcomeMessage');
+    if (outcome === 'craps') {
+        messageElement.textContent = 'Craps! You lose.';
+    } else if (outcome === 'natural') {
+        messageElement.textContent = 'Natural! You win.';
+    } else {
+        messageElement.textContent = `Point established: ${rolledValue}`;
+    }
+}
+
+// Function to simulate a craps game
+function playCraps() {
+    const rolledValue = rollDice();
+    const rollOutcome = determineRollOutcome(rolledValue);
+
+    // Update and display the roll counts
+    rollCounts[rolledValue]++;
+
+    // Display the outcome message
+    displayOutcomeMessage(rollOutcome);
+}
+
+// Attach a click event listener to the button to play the craps game
+document.getElementById('rollButton').addEventListener('click', playCraps);
