887c01ba80a59ac734f12047b66fd46a378b2d8f
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-05 22:29:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function rollDice() {
     const resultNumbers = resultElement.querySelectorAll('.number');
     const rolledValue = Math.floor(Math.random() * 12) + 1;
 
+    // Check if the animation class is already added, and remove it to reset the animation
+    resultElement.classList.remove('rolling');
+
     // Calculate the translateY value for animation
     const translateY = -(rolledValue - 1) * 40; // Adjust the multiplier for desired spacing
 
@@ -14,6 +17,11 @@ function rollDice() {
             number.style.transform = `translateY(${translateY}px)`;
         }, animationDelay);
     });
+
+    // Add the animation class to trigger the animation
+    setTimeout(() => {
+        resultElement.classList.add('rolling');
+    }, 10); // A small delay to ensure the class is added after resetting
 
     return rolledValue;
 }
