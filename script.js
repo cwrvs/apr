fade044919bb37fccfa8da7ce96a5fcf4cf3bcfe
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-24 23:21:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-// script.js
-function generateRandomNumber() {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById('number-result').innerText = "Random Number: " + randomNumber;
+function generateRandomNumbers() {
+    const min = parseInt(document.getElementById('min').value);
+    const max = parseInt(document.getElementById('max').value);
+    const count = Math.min(parseInt(document.getElementById('count').value), max - min + 1);
+    let numbers = [];
+    
+    while (numbers.length < count) {
+        let number = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numbers.includes(number)) {
+            numbers.push(number);
+        }
+    }
+    document.getElementById('number-result').innerText = "Random Numbers: " + numbers.join(', ');
 }
 
 function pickRandomName() {
     const names = document.getElementById('name-list').value.split('\n').filter(name => name.trim() !== '');
+    
     if (names.length === 0) {
         document.getElementById('name-result').innerText = "Please input names.";
         return;
     }
+    
     const randomName = names[Math.floor(Math.random() * names.length)];
     document.getElementById('name-result').innerText = "Selected Name: " + randomName;
 }
