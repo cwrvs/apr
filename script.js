0aa7403cb07828c802ee70c0c4c2910437d0a045
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-24 23:03:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // script.js
 function generateRandomNumber() {
-    const min = parseInt(document.getElementById('min').value);
-    const max = parseInt(document.getElementById('max').value);
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById('result').innerText = "Random Number: " + randomNumber;
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('number-result').innerText = "Random Number: " + randomNumber;
+}
+
+function pickRandomName() {
+    const names = document.getElementById('name-list').value.split('\n').filter(name => name.trim() !== '');
+    if (names.length === 0) {
+        document.getElementById('name-result').innerText = "Please input names.";
+        return;
+    }
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    document.getElementById('name-result').innerText = "Selected Name: " + randomName;
 }
