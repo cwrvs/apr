5b7f9ad736c7bfbb3f6b11361de61e5e48416a50
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-10-25 11:34:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const makeDecisionButton = document.getElementById('make-decision');
-    const decisionListInput = document.getElementById('decision-list');
-    const nonRepeatingNamesList = document.getElementById('non-repeating-names');
-    const numberedList = document.getElementById('numbered-list');
+    // Number Generator
+    const generateNumberButton = document.getElementById('generate-number');
+    const minInput = document.getElementById('min');
+    const maxInput = document.getElementById('max');
+    const randomNumberDisplay = document.getElementById('random-number');
 
-    makeDecisionButton.addEventListener('click', function() {
-        const optionList = decisionListInput.value.split(',').map(item => item.trim());
+    generateNumberButton.addEventListener('click', function() {
+        const min = parseInt(minInput.value);
+        const max = parseInt(maxInput.value);
         
-        if (optionList.length < 2) {
-            alert('Please enter at least two options.');
+        if (isNaN(min) || isNaN(max)) {
+            alert('Please enter valid numbers for both minimum and maximum values.');
             return;
         }
 
-        // Shuffle the optionList to randomize names
-        shuffleArray(optionList);
+        if (min >= max) {
+            alert('Minimum value must be less than the maximum value.');
+            return;
+        }
 
-        // Display non-repeating names on the left
-        displayNonRepeatingNames(optionList);
-
-        // Display numbered list on the right
-        displayNumberedList(optionList);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        randomNumberDisplay.textContent = `Random Number: ${randomNumber}`;
     });
 
-    // Shuffle an array to randomize elements
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
+    // Name Generator
+    const selectNameButton = document.getElementById('select-name');
+    const nameListInput = document.getElementById('name-list');
+    const selectedNameDisplay = document.getElementById('selected-name');
+
+    selectNameButton.addEventListener('click', function() {
+        const nameList = nameListInput.value.split(',').map(name => name.trim());
+        
+        if (nameList.length < 2) {
+            alert('Please enter at least two names in the list.');
+            return;
         }
-    }
 
-    // Display non-repeating names
-    function displayNonRepeatingNames(names) {
-        nonRepeatingNamesList.innerHTML = '';
-        names.forEach(name => {
-            const li = document.createElement('li');
-            li.textContent = name;
-            nonRepeatingNamesList.appendChild(li);
-        });
-    }
-
-    // Display numbered list
-    function displayNumberedList(names) {
-        numberedList.innerHTML = '';
-        names.forEach((name, index) => {
-            const li = document.createElement('li');
-            li.textContent = `${index + 1}. ${name}`;
-            numberedList.appendChild(li);
-        });
-    }
+        const randomIndex = Math.floor(Math.random() * nameList.length);
+        const selectedName = nameList[randomIndex];
+        selectedNameDisplay.textContent = `Selected Name: ${selectedName}`;
+    });
 });
