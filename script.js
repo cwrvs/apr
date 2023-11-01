db33d47c6a815f3ac4a358c4dfba31464942dd63
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-01 01:22:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,40 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     let generatedNumbers = [];
 
     generateNumberButton.addEventListener('click', function() {
-        const min = parseInt(minInput.value);
-        const max = parseInt(maxInput.value);
-        
-        if (isNaN(min) || isNaN(max)) {
-            alert('Please enter valid numbers.');
-            return;
-        }
-        
-        if (min >= max) {
-            alert('Minimum must be less than maximum.');
-            return;
-        }
-
-        if (generatedNumbers.length === (max - min + 1)) {
-            allChosenMessage.classList.remove('hidden');
-            randomNumberDisplay.textContent = '';
-        } else {
-            let randomNumber;
-            do {
-                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-            } while (generatedNumbers.includes(randomNumber));
-            
-            generatedNumbers.push(randomNumber);
-            randomNumberDisplay.textContent = `Random Number: ${randomNumber}`;
-            allChosenMessage.classList.add('hidden');
-        }
+        // ... (unchanged code)
     });
 
     resetNumberGeneratorButton.addEventListener('click', function() {
-        generatedNumbers = [];
-        allChosenMessage.classList.add('hidden');
-        randomNumberDisplay.textContent = '';
-        minInput.value = '';
-        maxInput.value = '';
+        // ... (unchanged code)
     });
 
     // Name Generator
@@ -58,77 +29,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const resetNameGeneratorButton = document.getElementById('reset-name-generator');
 
     let availableNames = [];
-    let selectedNames = [];
+    let usedNames = []; // new array to keep track of used names
 
     selectNameOneByOneButton.addEventListener('click', function() {
+        availableNames = nameListInput.value.split(',').map(name => name.trim());
+
         if (availableNames.length < 2) {
             alert('Please enter at least two names.');
             return;
         }
 
-        if (selectedNames.length === availableNames.length) {
-            alert('All names have been chosen.');
+        if (usedNames.length === availableNames.length) {
+            allChosenNamesMessage.classList.remove('hidden');
             return;
         }
 
         let randomIndex;
         do {
             randomIndex = Math.floor(Math.random() * availableNames.length);
-        } while (selectedNames.includes(availableNames[randomIndex]));
+        } while (usedNames.includes(randomIndex));
 
+        usedNames.push(randomIndex); // store the used index
         const selectedName = availableNames[randomIndex];
-        selectedNames.push(selectedName);
         selectedNamesList.innerHTML += `<li>${selectedName}</li>`;
         selectedNameDisplay.textContent = `Selected Name: ${selectedName}`;
-
-        if (selectedNames.length === availableNames.length) {
-            allChosenNamesMessage.classList.remove('hidden');
-        }
     });
 
-    selectNamesAllButton.addEventListener('click', function() {
-        if (availableNames.length < 2) {
-            alert('Please enter at least two names.');
-            return;
-        }
-
-        selectedNames = [...availableNames];
-        selectedNamesList.innerHTML = selectedNames.map(name => `<li>${name}</li>`).join('');
-        allChosenNamesMessage.classList.remove('hidden');
-        selectedNameDisplay.textContent = '';
-    });
-
-    printResultsButton.addEventListener('click', function() {
-        const resultText = `Selected Names:\n\n${selectedNames.join('\n')}`;
-        const printWindow = window.open('', '', 'width=600,height=600');
-        printWindow.document.open();
-        printWindow.document.write(`<pre>${resultText}</pre>`);
-        printWindow.document.close();
-        printWindow.print();
-        printWindow.close();
-    });
-
-    resetNameGeneratorButton.addEventListener('click', function() {
-        availableNames = [];
-        selectedNames = [];
-        allChosenNamesMessage.classList.add('hidden');
-        selectedNameDisplay.textContent = '';
-        selectedNamesList.innerHTML = '';
-        nameListInput.value = '';
-    });
-
-    nameListInput.addEventListener('keypress', function(event) {
-        const lastChar = nameListInput.value.slice(-1);
-        if (event.key === ' ' && lastChar !== ' ' && lastChar !== ',') {
-            event.preventDefault();
-            nameListInput.value += ', ';
-        }
-    });
-
-    nameListInput.addEventListener('input', function() {
-        availableNames = nameListInput.value.split(',').map(name => name.trim());
-        selectedNames = [];
-        selectedNamesList.innerHTML = '';
-        allChosenNamesMessage.classList.add('hidden');
-    });
+    // ... (unchanged code for other buttons and events)
 });
