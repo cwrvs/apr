0d960b5177b9ae51a1c1b0a01ad5f1d77cc38fb8
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-15 20:59:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Random Number Generator
-  const minInput = document.getElementById("min");
-  const maxInput = document.getElementById("max");
-  const generateNumberBtn = document.getElementById("generate-number");
-  const randomNumberOutput = document.getElementById("random-number");
-  const allChosenOutput = document.getElementById("all-chosen");
-  const resetNumberGeneratorBtn = document.getElementById("reset-number-generator");
+function calculateLoan() {
+    var amount = document.getElementById('amount').value;
+    var interestRate = document.getElementById('interestRate').value;
+    var loanTerm = document.getElementById('loanTerm').value;
+    var payoffMonths = document.getElementById('payoffMonths').value;
+    var extraPayment = document.getElementById('extraPayment').value;
 
-  generateNumberBtn.addEventListener("click", function () {
-    const min = parseInt(minInput.value);
-    const max = parseInt(maxInput.value);
+    // Convert annual interest rate to monthly and decimal form
+    var monthlyInterestRate = (interestRate / 100) / 12;
+    
+    // Calculate standard monthly payment without extra payment
+    var standardPayment = amount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTerm)) / (Math.pow(1 + monthlyInterestRate, loanTerm) - 1);
 
-    if (!isNaN(min) && !isNaN(max)) {
-      const randomNum = getRandomNumber(min, max);
-      randomNumberOutput.textContent = `Random Number: ${randomNum}`;
+    // Initialize variables for calculation with extra payment
+    var currentBalance = amount;
+    var totalInterestPaid = 0;
+    var month = 0;
+
+    // Calculate loan payoff with extra payments
+    while (currentBalance > 0 && month < payoffMonths) {
+        var interestForThisMonth = currentBalance * monthlyInterestRate;
+        var principalForThisMonth = standardPayment - interestForThisMonth + parseFloat(extraPayment);
+        if (principalForThisMonth > currentBalance) {
+            principalForThisMonth = currentBalance; // Prevent overpayment
+        }
+        currentBalance -= principalForThisMonth;
+        totalInterestPaid += interestForThisMonth;
+        month++;
     }
-  });
 
-  resetNumberGeneratorBtn.addEventListener("click", function () {
-    minInput.value = "";
-    maxInput.value = "";
-    randomNumberOutput.textContent = "";
-    allChosenOutput.classList.add("hidden");
-  });
+    var resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = 'Total Interest Paid: ' + totalInterestPaid.toFixed(2) + '<br/>' + 
+                           'Total Months: ' + month;
+}
 
-  // Random Name Selector
-  const nameListInput = document.getElementById("name-list");
-  const selectNameBtn = document.getElementById("select-name");
-  const selectNamesAllBtn = document.getElementById("select-names-all");
-  const printResultsBtn = document.getElementById("print-results");
-  const selectedNameOutput = document.getElementById("selected-name");
-  const selectedNamesList = document.getElementById("selected-names-list");
-  const allChosenNamesOutput = document.getElementById("all-chosen-names");
-  const resetNameGeneratorBtn = document.getElementById("reset-name-generator");
-  const nameFileInput = document.getElementById("name-file");
-
-  selectNameBtn.addEventListener("click", function () {
-    const names = parseNames(nameListInput.value);
-
-    if (names.length > 0) {
-      const randomIndex = getRandomNumber(0, names.length - 1);
-      const selectedName = names[randomIndex];
-      selectedNameOutput.textContent = `Selected Name: ${selectedName}`;
-      names.splice(randomIndex, 1); // Remove the selected name
-      if (names.length === 0) {
-        allChosenNamesOutput.classList.remove("hidden");
-      }
+// Populate Payoff Months dropdown
+window.onload = function() {
+    var select = document.getElementById('payoffMonths');
+    for (var i = 12; i <= 239; i++) {
+        var opt = document.createElement('option');
+        opt.value = i;
+        opt.innerHTML = i + ' months';
+        select.appendChild(opt);
     }
-  });
-
-  selectNamesAllBtn.addEventListener("click", function () {
-    const names = parseNames(nameListInput.value);
-    selectedNamesList.innerHTML = ""; // Clear previous results
-
-    if (names.length > 0) {
-      shuffleArray(names); // Shuffle the array of names
-      names.forEach((name) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = name;
-        selectedNamesList.appendChild(listItem);
-      });
-      allChosenNamesOutput.classList.remove("hidden");
-    }
-  });
-
-  printResultsBtn.addEventListener("click", function () {
-    window.print();
-  });
-
-  resetNameGeneratorBtn.addEventListener("click", function () {
-    nameListInput.value = "";
-    selectedNameOutput.textContent = "";
-    selectedNamesList.innerHTML = "";
-    allChosenNamesOutput.classList.add("hidden");
-  });
-
-  // Handle file upload
-  nameFileInput.addEventListener("change", function () {
-    const file = nameFileInput.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        nameListInput.value = e.target.result;
-      };
-      reader.readAsText(file);
-    }
-  });
-
-  function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-    }
-  }
-
-  function parseNames(input) {
-    const names = input
-      .split(/\n|,|, /) // Split by newline, comma, or comma + space
-      .map((name) => name.trim())
-      .filter((name) => name !== "");
-    return names;
-  }
-});
+};
