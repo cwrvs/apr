6e3b2e0983f465bdf844773366d8e26aa461df90
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-17 15:06:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -34,4 +34,41 @@ function calculateLoan() {
 
     var nextTermPayment = calculatePayment(nextTerm);
     var prevTermPayment = calculatePayment(prevTerm);
- 
+    var nextTermTotalInterest = nextTermPayment * nextTerm - amount;
+    var prevTermTotalInterest = prevTermPayment * prevTerm - amount;
+
+    var resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = `
+        <div class='result-highlight'>
+            <h3>Extra Payment and Effective Interest Rate:</h3>
+            <p>Extra Monthly Payment: ${extraPayment.toFixed(2)}</p>
+            <p>Effective Interest Rate: ${effectiveInterestRate.toFixed(2)}%</p>
+            <p>Interest Saved by Extra Payments: ${interestSaved.toFixed(2)}</p>
+        </div>
+        <div class='result-section'>
+            <h3>Selected Term (${loanTerm} months):</h3>
+            <p>Monthly Payment: ${standardPayment.toFixed(2)}</p>
+            <p>Total Interest: ${totalInterestWithoutExtra.toFixed(2)}</p>
+        </div>
+        <div class='result-section'>
+            <h3>Next Term (${nextTerm} months):</h3>
+            <p>Monthly Payment: ${nextTermPayment.toFixed(2)} (${(nextTermPayment - standardPayment).toFixed(2)} difference)</p>
+            <p>Total Interest: ${nextTermTotalInterest.toFixed(2)} (${(nextTermTotalInterest - totalInterestWithoutExtra).toFixed(2)} difference)</p>
+        </div>
+        <div class='result-section'>
+            <h3>Previous Term (${prevTerm} months):</h3>
+            <p>Monthly Payment: ${prevTermPayment.toFixed(2)} (${(prevTermPayment - standardPayment).toFixed(2)} difference)</p>
+            <p>Total Interest: ${prevTermTotalInterest.toFixed(2)} (${(prevTermTotalInterest - totalInterestWithoutExtra).toFixed(2)} difference)</p>
+        </div>`;
+}
+
+window.onload = function() {
+    var select = document.getElementById('loanTerm');
+    var terms = [84, 96, 120, 144, 180, 204, 240];
+    terms.forEach(term => {
+        var opt = document.createElement('option');
+        opt.value = term;
+        opt.innerHTML = term + ' months';
+        select.appendChild(opt);
+    });
+};
