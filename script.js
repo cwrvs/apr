cb7f337a5b6792e918a7479d7a706f86ccdd754b
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-17 12:18:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 function calculateLoan() {
-    var amount = parseFloat(document.getElementById('amount').value);
-    var interestRate = parseFloat(document.getElementById('interestRate').value);
-    var loanTerm = parseInt(document.getElementById('loanTerm').value, 10);
-    var extraPayment = parseFloat(document.getElementById('extraPayment').value);
+    // ... existing code ...
 
-    var monthlyInterestRate = (interestRate / 100) / 12;
-    var standardPayment = amount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
-    
-    var currentBalance = amount;
-    var totalInterestPaid = 0;
-    var month = 0;
-    var totalInterestForOriginalSchedule = standardPayment * loanTerm - amount;
-    var interestSaved = 0;
-
-    while (currentBalance > 0) {
-        var interestForThisMonth = currentBalance * monthlyInterestRate;
-        var principalForThisMonth = Math.min(standardPayment - interestForThisMonth + extraPayment, currentBalance);
-        currentBalance -= principalForThisMonth;
-        totalInterestPaid += interestForThisMonth;
-        month++;
-        if (month >= loanTerm) break; // Avoid infinite loop if balance never reaches 0
+    // Function to calculate payment and total interest for a given term
+    function calculateForTerm(term) {
+        var monthlyInterestRate = (interestRate / 100) / 12;
+        var standardPayment = amount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -term));
+        var totalInterest = standardPayment * term - amount;
+        return { standardPayment, totalInterest };
     }
 
-    var effectiveInterestRate = interestRate; // Default to nominal interest rate
-    if (extraPayment > 0) {
-        effectiveInterestRate = (totalInterestPaid / amount) / (month / 12) * 100;
-        interestSaved = totalInterestForOriginalSchedule - totalInterestPaid;
-    }
+    // Calculate for current, shorter, and longer terms
+    var currentTermDetails = calculateForTerm(loanTerm);
+    var shorterTermDetails = loanTerm > 12 ? calculateForTerm(loanTerm - 12) : null; // Check if shorter term is valid
+    var longerTermDetails = calculateForTerm(loanTerm + 12); // Longer term
 
+    // Display current term results
     var resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = 'Standard Monthly Payment: ' + standardPayment.toFixed(2) + '<br/>' +
-                           'Total Interest Paid with Extra Payments: ' + totalInterestPaid.toFixed(2) + '<br/>' +
-                           'Total Interest Without Extra Payments: ' + totalInterestForOriginalSchedule.toFixed(2) + '<br/>' +
-                           'Interest Saved by Extra Payments: ' + interestSaved.toFixed(2) + '<br/>' +
-                           'Total Months to Pay Off: ' + month + '<br/>' +
-                           'Effective Interest Rate with Extra Payments: ' + effectiveInterestRate.toFixed(2) + '%';
+    resultsDiv.innerHTML = 'Current Term (' + loanTerm + ' months):<br/>' +
+                           'Monthly Payment: ' + currentTermDetails.standardPayment.toFixed(2) + '<br/>' +
+                           'Total Interest: ' + currentTermDetails.totalInterest.toFixed(2) + '<br/>';
+
+    // Display shorter term results if applicable
+    if (shorterTermDetails) {
+        resultsDiv.innerHTML += '<br/>Shorter Term (' + (loanTerm - 12) + ' months):<br/>' +
+                                'Monthly Payment: ' + shorterTermDetails.standardPayment.toFixed(2) + '<br/>' +
+                                'Total Interest: ' + shorterTermDetails.totalInterest.toFixed(2) + '<br/>';
+    }
+
+    // Display longer term results
+    resultsDiv.innerHTML += '<br/>Longer Term (' + (loanTerm + 12) + ' months):<br/>' +
+                            'Monthly Payment: ' + longerTermDetails.standardPayment.toFixed(2) + '<br/>' +
+                            'Total Interest: ' + longerTermDetails.totalInterest.toFixed(2);
+
+    // ... existing code to handle effective interest rate and other calculations ...
 }
 
 window.onload = function() {
