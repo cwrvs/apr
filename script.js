1e3bc5c80d9044b60cad757d7ed2e50bcab30cbf
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-17 12:29:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function calculateLoan() {
-    // ... existing code ...
+    var amount = parseFloat(document.getElementById('amount').value) || 0;
+    var interestRate = parseFloat(document.getElementById('interestRate').value) || 0;
+    var loanTerm = parseInt(document.getElementById('loanTerm').value, 10) || 0;
+    var extraPayment = parseFloat(document.getElementById('extraPayment').value) || 0;
 
     // Function to calculate payment and total interest for a given term
     function calculateForTerm(term) {
@@ -14,11 +17,36 @@ function calculateLoan() {
     var shorterTermDetails = loanTerm > 12 ? calculateForTerm(loanTerm - 12) : null; // Check if shorter term is valid
     var longerTermDetails = calculateForTerm(loanTerm + 12); // Longer term
 
+    var currentBalance = amount;
+    var totalInterestPaid = 0;
+    var month = 0;
+    var totalInterestForOriginalSchedule = currentTermDetails.standardPayment * loanTerm - amount;
+    var interestSaved = 0;
+
+    while (currentBalance > 0) {
+        var interestForThisMonth = currentBalance * monthlyInterestRate;
+        var principalForThisMonth = Math.min(currentTermDetails.standardPayment - interestForThisMonth + extraPayment, currentBalance);
+        currentBalance -= principalForThisMonth;
+        totalInterestPaid += interestForThisMonth;
+        month++;
+        if (month >= loanTerm) break; // Avoid infinite loop if balance never reaches 0
+    }
+
+    var effectiveInterestRate = interestRate; // Default to nominal interest rate
+    if (extraPayment > 0) {
+        effectiveInterestRate = (totalInterestPaid / amount) / (month / 12) * 100;
+        interestSaved = totalInterestForOriginalSchedule - totalInterestPaid;
+    }
+
     // Display current term results
     var resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = 'Current Term (' + loanTerm + ' months):<br/>' +
                            'Monthly Payment: ' + currentTermDetails.standardPayment.toFixed(2) + '<br/>' +
-                           'Total Interest: ' + currentTermDetails.totalInterest.toFixed(2) + '<br/>';
+                           'Total Interest: ' + currentTermDetails.totalInterest.toFixed(2) + '<br/>' +
+                           'Total Interest Paid with Extra Payments: ' + totalInterestPaid.toFixed(2) + '<br/>' +
+                           'Interest Saved by Extra Payments: ' + interestSaved.toFixed(2) + '<br/>' +
+                           'Total Months to Pay Off: ' + month + '<br/>' +
+                           'Effective Interest Rate with Extra Payments: ' + effectiveInterestRate.toFixed(2) + '%';
 
     // Display shorter term results if applicable
     if (shorterTermDetails) {
@@ -31,8 +59,6 @@ function calculateLoan() {
     resultsDiv.innerHTML += '<br/>Longer Term (' + (loanTerm + 12) + ' months):<br/>' +
                             'Monthly Payment: ' + longerTermDetails.standardPayment.toFixed(2) + '<br/>' +
                             'Total Interest: ' + longerTermDetails.totalInterest.toFixed(2);
-
-    // ... existing code to handle effective interest rate and other calculations ...
 }
 
 window.onload = function() {
@@ -43,6 +69,6 @@ window.onload = function() {
         opt.innerHTML = i + ' months';
         select.appendChild(opt);
     }
-    // Preselect a loan term, if needed
+    // Optionally preselect a loan term
     // document.getElementById('loanTerm').value = 120;
 };
