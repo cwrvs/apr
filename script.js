44460ab88343e4bffa19f392068f639ca690d0c2
cwrvs <147121550+cwrvs@users.noreply.github.com>
2023-11-17 19:03:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,52 +14,33 @@ function calculateLoan() {
 
     var currentBalance = amount;
     var totalInterestPaidWithExtra = 0;
-    var month = 0;
+    var monthsWithExtra = 0;
     while (currentBalance > 0) {
         var interestForThisMonth = currentBalance * monthlyInterestRate;
         var principalForThisMonth = Math.min(standardPayment - interestForThisMonth + extraPayment, currentBalance);
         currentBalance -= principalForThisMonth;
         totalInterestPaidWithExtra += interestForThisMonth;
-        month++;
-        if (month >= loanTerm) break;
+        monthsWithExtra++;
+        if (monthsWithExtra >= loanTerm) break;
     }
 
-    var effectiveInterestRate = (extraPayment > 0) ? (totalInterestPaidWithExtra / amount) / (month / 12) * 100 : interestRate;
+    var effectiveInterestRate = (extraPayment > 0) ? (totalInterestPaidWithExtra / amount) / (monthsWithExtra / 12) * 100 : interestRate;
     var interestSaved = totalInterestWithoutExtra - totalInterestPaidWithExtra;
-
-    var adjacentTerms = [84, 96, 120, 144, 180, 204, 240];
-    var currentTermIndex = adjacentTerms.indexOf(loanTerm);
-    var nextTerm = adjacentTerms[currentTermIndex + 1] || loanTerm;
-    var prevTerm = adjacentTerms[currentTermIndex - 1] || loanTerm;
-
-    var nextTermPayment = calculatePayment(nextTerm);
-    var prevTermPayment = calculatePayment(prevTerm);
-    var nextTermTotalInterest = nextTermPayment * nextTerm - amount;
-    var prevTermTotalInterest = prevTermPayment * prevTerm - amount;
+    var monthsReduced = loanTerm - monthsWithExtra;
 
     var resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = `
         <div class='result-highlight'>
-            <h3>Extra Payment and Effective Interest Rate:</h3>
+            <h3>Extra Payment Details:</h3>
             <p>Extra Monthly Payment: ${extraPayment.toFixed(2)}</p>
             <p>Effective Interest Rate: ${effectiveInterestRate.toFixed(2)}%</p>
-            <p>Interest Saved by Extra Payments: ${interestSaved.toFixed(2)}</p>
+            <p>Interest Saved: ${interestSaved.toFixed(2)}</p>
+            <p>Loan Duration Reduced By: ${monthsReduced} months</p>
         </div>
-        <div class='result-section'>
-            <h3>Selected Term (${loanTerm} months):</h3>
-            <p>Monthly Payment: ${standardPayment.toFixed(2)}</p>
-            <p>Total Interest: ${totalInterestWithoutExtra.toFixed(2)}</p>
-        </div>
-        <div class='result-section'>
-            <h3>Next Term (${nextTerm} months):</h3>
-            <p>Monthly Payment: ${nextTermPayment.toFixed(2)} (${(nextTermPayment - standardPayment).toFixed(2)} difference)</p>
-            <p>Total Interest: ${nextTermTotalInterest.toFixed(2)} (${(nextTermTotalInterest - totalInterestWithoutExtra).toFixed(2)} difference)</p>
-        </div>
-        <div class='result-section'>
-            <h3>Previous Term (${prevTerm} months):</h3>
-            <p>Monthly Payment: ${prevTermPayment.toFixed(2)} (${(prevTermPayment - standardPayment).toFixed(2)} difference)</p>
-            <p>Total Interest: ${prevTermTotalInterest.toFixed(2)} (${(prevTermTotalInterest - totalInterestWithoutExtra).toFixed(2)} difference)</p>
-        </div>`;
+        <!-- ... code for displaying other loan terms ... -->
+    `;
+
+    // ... rest of your code for displaying selected term, next term, and previous term ...
 }
 
 window.onload = function() {
